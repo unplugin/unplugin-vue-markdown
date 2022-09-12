3cbcf18ba2018b65daf898165a25b4f8df396e9a
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-12 12:46:59 +0800

    fix: exclude vue from build

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
 export function getVueVersion(defaultVersion = '3.2.0') {
   try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    let v = require('vue')
+    const _require = require
+    let v = _require('vue')
     if (v.default)
       v = v.default
     return v.version || defaultVersion
