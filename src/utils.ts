0d666808a18e9c324917738b4a30e9924739a64c
Anthony Fu <anthonyfu117@hotmail.com>
2022-12-30 13:23:13 +0100

    chore: lint

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 export function getVueVersion(defaultVersion = '3.2.0') {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const _require = require
     let v = _require('vue')
     if (v.default)
