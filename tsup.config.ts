4837d6de49722de79c262f707c825447b9e177f3
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-05-05 16:54:01 +0900

    fix: change target instead of esbuildOptions.support

--- tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  esbuildOptions(options) {
-    options.supported ??= {}
-    options.supported['import-meta'] = true
-  },
-})
