2f1f281bab265e4d7781703bfd3afdd7f9e8fbef
Anthony Fu <anthonyfu117@hotmail.com>
2023-05-07 20:41:38 +0200

    fix: improve frontmatter handling

--- src/head.ts
+++ src/head.ts
@@ -24,19 +24,28 @@ export function preprocessHead<T extends Frontmatter>(frontmatter: T, options: R
   if (head.title) {
     if (!meta.find((i: any) => i.property === 'og:title'))
       meta.push({ property: 'og:title', content: head.title })
+
+    if (!meta.find((i: any) => i.name === 'twitter:title'))
+      meta.push({ name: 'twitter:title', content: head.title })
   }
 
   if (head.description) {
+    if (!meta.find((i: any) => i.name === 'description'))
+      meta.push({ name: 'description', content: head.description })
+
     if (!meta.find((i: any) => i.property === 'og:description'))
       meta.push({ property: 'og:description', content: head.description })
 
-    if (!meta.find((i: any) => i.name === 'description'))
-      meta.push({ name: 'description', content: head.description })
+    if (!meta.find((i: any) => i.name === 'twitter:description'))
+      meta.push({ name: 'twitter:description', content: head.description })
   }
 
   if (head.image) {
     if (!meta.find((i: any) => i.property === 'og:image'))
       meta.push({ property: 'og:image', content: head.image })
+
+    if (!meta.find((i: any) => i.name === 'twitter:image'))
+      meta.push({ name: 'twitter:image', content: head.image })
 
     if (!meta.find((i: any) => i.property === 'twitter:card'))
       meta.push({ name: 'twitter:card', content: 'summary_large_image' })
