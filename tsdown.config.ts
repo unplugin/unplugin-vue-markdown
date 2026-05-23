5346b61373e35e47b1fb2fda6a9d33952f61a2b7
Anthony Fu <github@antfu.me>
2026-05-23 12:09:30 +0900

    fix: include dist in published package

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/*.ts'],
   format: 'esm',
-  fixedExtension: false,
   dts: true,
   clean: true,
+  exports: true
 })
